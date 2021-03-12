27b914b77fe3e28a740bc64d9ac1be4f5afb6a79
BurdetteLamar <burdettelamar@yahoo.com>
2021-03-12 09:15:54 -0600

    More

--- test/rdoc_toc_test.rb
+++ test/rdoc_toc_test.rb
@@ -10,29 +10,25 @@ class RDocTocTest < Minitest::Test
         empty
         six_levels
       /.each do |file_base_name|
-      rdoc_file_name = file_base_name + '.rdoc'
-      toc_file_name = file_base_name + '.toc'
-      rdoc_file_path = File.join('test', 'files', rdoc_file_name)
-      exp_toc_file_path = File.join('test', 'files', toc_file_name)
-      Dir.mktmpdir do |tmp_dir_path|
-        act_toc_file_path = File.join(tmp_dir_path, toc_file_name)
-        RDocToc.toc(rdoc_file_path, act_toc_file_path)
-        assert_files(exp_toc_file_path, act_toc_file_path, file_base_name)
-      end
+      do_good(file_base_name)
     end
   end
 
   def test_title
-    file_base_name = 'title'
+    do_good('title', {title: 'Contents'})
+  end
+
+  def do_good(file_base_name, options = {})
     rdoc_file_name = file_base_name + '.rdoc'
     toc_file_name = file_base_name + '.toc'
     rdoc_file_path = File.join('test', 'files', rdoc_file_name)
     exp_toc_file_path = File.join('test', 'files', toc_file_name)
     Dir.mktmpdir do |tmp_dir_path|
       act_toc_file_path = File.join(tmp_dir_path, toc_file_name)
-      RDocToc.toc(rdoc_file_path, act_toc_file_path, {title: 'Contents'})
+      RDocToc.toc(rdoc_file_path, act_toc_file_path, options)
       assert_files(exp_toc_file_path, act_toc_file_path, file_base_name)
     end
+
   end
 
   def assert_files(exp_file_path, act_file_path, file_base_name)
@@ -40,8 +36,6 @@ class RDocTocTest < Minitest::Test
       assert(true)
       return
     end
-    p exp_file_path
-    p act_file_path
     exp_toc = File.read(exp_file_path)
     act_toc = File.read(act_file_path)
     assert_equal(exp_toc, act_toc, file_base_name)
